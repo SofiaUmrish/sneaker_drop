c6cd63f69d95fd29fe7f0b8c8d032ecd764603e8
SofiaUmrish <git@github.com:SofiaUmrish/labs_ap.git>
2025-12-23 18:08:21 +0200

    fix api url for mobile

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,7 @@
 const config = {
-    API_BASE_URL: 'http://localhost:5000/api'
+    API_BASE_URL: window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
+        ? 'http://localhost:5000/api'
+        : '/api'
 };
 
 export default config;
